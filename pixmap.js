8e4ee0443a9b6f43d016e6302843386ca99f8dbe
Ilya Shveikin <no_soap_0@mail.ru>
2021-04-05 22:37:29 +0300

    12

--- pixmap.js
+++ pixmap.js
@@ -7,17 +7,22 @@ class pixmap {
         this.wrapper = document.createElement('div');
         this.wrapper.style.width = sizex * pixelSize + sizex + 'px';
         this.wrapper.style.height = sizey * pixelSize + sizey + 1 + 'px';
+        this.wrapper.style.position = 'relative'
         this.pixels = []
         this.pixels2d = {}
         for(let yi = 0; yi < sizey; yi++){
             for(let xi = 0; xi < sizex; xi++){
                 const pixel = document.createElement('div')
-                pixel.style.width = pixelSize + 'px';
-                pixel.style.height = pixelSize + 'px';
-                pixel.style.border = '1px solid #000';
-                pixel.style.display = 'inline-block'
-                pixel.style.marginRight = '-1px'; 
-                pixel.style.marginBottom = '-5px'; 
+                pixel.style.width = pixelSize + 'px'
+                pixel.style.height = pixelSize + 'px'
+                pixel.style.border = '1px solid #000'
+                pixel.style.top = yi * this.pixelSize - yi + 'px'
+                pixel.style.left = xi * this.pixelSize - xi + 'px'
+                pixel.style.position = 'absolute'
+                pixel.style.boxSizing = 'border-box'
+
+
+                
 
                 this.wrapper.appendChild(pixel);
                 this.pixels.push(pixel)
@@ -26,7 +31,19 @@ class pixmap {
                 this.pixels2d[xi][yi] = pixel
             }
         }
+    }
+
+    rand(){
+        return [Math.floor(Math.random() * this.sizex), Math.floor(Math.random() * this.sizey)];
+    }
+
+    set(x, y, color){
+        this.pixels2d[x][y].style.background = color
+    }
+
+    aset([x,y], color){
         
+        this.pixels2d[x][y].style.background = color
     }
 
     getElement(){
