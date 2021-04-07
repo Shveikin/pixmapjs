44f31e4ee7ba77e52e98d891576eef01a4dc24b4
picus23 <picus.2022@mail.ru>
2021-04-07 11:31:18 +0300

    1 is color

--- pixmap.js
+++ pixmap.js
@@ -9,20 +9,26 @@ class pixmap {
         this.wrapper.style.height = sizey * pixelSize + sizey + 1 + 'px';
         this.wrapper.style.position = 'relative'
         this.pixels = []
+        this.bites = []
+
         this.pixels2d = {}
         for(let yi = 0; yi < sizey; yi++){
             for(let xi = 0; xi < sizex; xi++){
+                this.bites.push(0)
                 const pixel = document.createElement('div')
                 pixel.style.width = pixelSize + 'px'
                 pixel.style.height = pixelSize + 'px'
-                pixel.style.border = '1px solid #000'
+                pixel.style.fontSize = pixelSize + 'px'
+                pixel.style.textAlign = 'center'
+                pixel.style.border = '1px solid #222'
                 pixel.style.top = yi * this.pixelSize - yi + 'px'
                 pixel.style.left = xi * this.pixelSize - xi + 'px'
                 pixel.style.position = 'absolute'
                 pixel.style.boxSizing = 'border-box'
+                pixel.style.background = '#000'
 
 
-                this.wrapper.appendChild(pixel);
+                this.wrapper.appendChild(pixel)
                 this.pixels.push(pixel)
 
                 if (!(xi in this.pixels2d)) this.pixels2d[xi] = {}
@@ -35,11 +41,28 @@ class pixmap {
         return [Math.floor(Math.random() * this.sizex), Math.floor(Math.random() * this.sizey)];
     }
 
-
     set([x,y], color, vector = ''){
-        this.pixels2d[x][y].style.background = color
+        if (color[0]=='#')
+            this.pixels2d[x][y].style.background = color
+        else
+            if (color==0){
+                this.pixels2d[x][y].style.background = '#444'
+                const pos = y * (this.sizex) + x;
+                this.bites[pos] = 0
+            } else {
+                this.pixels2d[x][y].style.background = '#fff'
+                const pos = y * (this.sizex) + x;
+                this.bites[pos] = 1
+            }
+            
+
+
         if (vector!='')
             this.pixels2d[x][y].innerHTML = vector
+    }
+
+    bite(){
+        return this.bites
     }
 
     getElement(){
